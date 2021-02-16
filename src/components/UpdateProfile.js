798558de3200ca7559ac7e8fac100363e0994b4c
samsamsam69 <shawnmartin0908@gmail.com>
2021-02-16 17:37:01 +0000

    This is the final commit

---
+++ src/components/UpdateProfile.js
@@ -0,0 +1,74 @@
+import React, {useRef, useState} from 'react';
+import {Form, Button, Card, Alert} from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom';
+import {useAuth} from '../contexts/AuthContext';
+
+export default function UpdateProfile() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const {currentUser, updatePassword, updateEmail} = useAuth();
+    const[error, setError] = useState('');
+    const[loading, setLoading] = useState(false);
+    const history = useHistory();
+
+    //triggered when submit button is pressed, aka submit event is triggered
+     function handleSubmit(e){
+
+        //prevents form from refreshing
+        e.preventDefault();
+        setLoading(true);
+        setError('');
+        //validation for password match or not
+        if(passwordRef.current.value !== passwordConfirmRef.current.value){
+            //return an Error State, so it will stop exit from the handleSubmit function
+            return setError('Passwords do not match');
+        }
+
+        const promises = [];
+        if (emailRef.current.value !== currentUser.email) {
+            promises.push(updateEmail(emailRef.current.value));
+        }
+        if (passwordRef.current.value){
+            promises.push(updatePassword(passwordRef.current.value))
+        }
+
+        Promise.all(promises).then(() => {
+            history.push('/');
+        }).catch(() => {
+            setError('Fail to update account');
+        }).finally(() => {
+            setLoading(false);
+        })
+
+
+    }
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Update Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            {/* type is the type of text field/control */}
+                            {/* ref is like "name" in HTML forms, used to get the information from the form when the form is submitted */}
+                            <Form.Control type="email" ref={emailRef} required defaultValue={currentUser.email}></Form.Control>
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} placeholder="Leave blank to keep the same"></Form.Control>
+                        </Form.Group>
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmRef}  placeholder="Leave blank to keep the same"></Form.Control>
+                        </Form.Group>
+                        {/* If the loadingState is true, button will be disabled */}
+                        <Button disabled={loading} className="w-100" type="submit">Update</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
